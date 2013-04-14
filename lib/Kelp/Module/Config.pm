652612d05da74191761e31c83d2046640bec12ea
Stefan G. <minimal@cpan.org>
2013-04-14 01:05:22 +0000

    Import of MINIMAL/Kelp-0.2 from CPAN.
    gitpan-cpan-distribution: Kelp
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         MINIMAL/Kelp-0.2.tar.gz
    gitpan-cpan-author:       MINIMAL
    gitpan-cpan-maturity:     released

--- lib/Kelp/Module/Config.pm
+++ lib/Kelp/Module/Config.pm
@@ -1,58 +1,41 @@
 package Kelp::Module::Config;
-
 use Kelp::Base 'Kelp::Module';
-use Config::Hash;
+use Carp;
+use Try::Tiny;
 
-sub build {
-    my ( $self, %args ) = @_;
+# Extension to look for
+attr ext => 'pl';
 
-    # Add a reference to the app in the config param hash
-    $args{param}->{app} = $self->app;
+# Directory where config files are
+attr path => sub {
+    my $self = shift;
+    return [
+        $ENV{KELP_CONFIG_DIR},
+        $self->app->path,
+        $self->app->path . '/conf',
+        $self->app->path . '../conf'
+    ]
+};
 
-    # Look for the config file
-    my $filename = 'conf/config.pl';
-    for ("", "/..") {
-        my $path = $self->app->path . "${_}/conf";
-        if ( -r "$path/config.pl" ) {
-            $filename = "$path/config.pl";
-            last;
-        }
-    }
+attr separator => qr/\./;
 
-    # Create the config object with some default attributes,
-    # but have %args at the end to trump all.
-    my $config   = Config::Hash->new(
-        data     => $self->defaults,
-        mode     => $self->app->mode,
-        filename => $filename,
-        %args
-    );
-
-    # Register two methods: config and config_hash
-    $self->register(
-        config_hash => $config->data,
-        config      => sub {
-            my ( $app, $path ) = @_;
-            return $config->get($path);
-        }
-    );
-}
-
-sub defaults {
+# Defaults
+attr data => sub {
     my $self = shift;
 
+    # Encoding
     my $encoding = 'UTF-8';
 
     # Return a big hash with default values
-    my $result = {
+    return {
 
-        # Default charset set to UTF-8
+        # Default charset is UTF-8
         charset => $encoding,
 
         app_url => 'http://localhost:5000',
 
         # Modules to load
-        modules => [qw/JSON Template Logger/],
+        modules => [qw/JSON Template/],
 
         # Module initialization params
         modules_init => {
@@ -67,29 +50,6 @@ sub defaults {
                 INCLUDE_PATH => [
                     $self->app->path . '/views',
                     $self->app->path . '/../views'
-                ]
-            },
-
-            # Logger - Default config is for development
-            Logger => {
-                outputs => [
-                    [
-                        'File',
-                        name      => 'debug',
-                        filename  => $self->app->path . '/log/debug.log',
-                        min_level => 'debug',
-                        mode      => '>>',
-                        newline   => 1,
-                        binmode   => ":encoding($encoding)"
-                    ], [
-                        'File',
-                        name      => 'error',
-                        filename  => $self->app->path . '/log/error.log',
-                        min_level => 'error',
-                        mode      => '>>',
-                        newline   => 1,
-                        binmode   => ":encoding($encoding)"
-                    ],
                 ]
             },
 
@@ -108,8 +68,116 @@ sub defaults {
         middleware_init => {},
 
     };
+};
 
-    return $result;
+sub get {
+    my ( $self, $path ) = @_;
+    return unless $path;
+    my @a = split( $self->separator, $path );
+    my $val = $self->data;
+    for my $chunk (@a) {
+        if ( ref($val) eq 'HASH' ) {
+            $val = $val->{$chunk};
+        }
+        else {
+            croak "Config path $path breaks at '$chunk'";
+        }
+    }
+    return $val;
+}
+
+# Override this one to use other config formats.
+sub load {
+    my ( $self, $filename ) = @_;
+
+    # Open and read file
+    open( my $in, "<:encoding(UTF-8)", $filename )
+      or do {
+        warn "Can not read config file " . $filename;
+        return {};
+      };
+
+    my $text = do { local $/ = undef; <$in> };
+    close($in);
+
+    my $_eval = sub {
+        local $@;
+        return (eval shift, $@);
+    };
+
+    my ( $hash, $error ) = $_eval->( $text );
+    die "Config file $filename parse error: " . $error if $error;
+    die "Config file $filename did not return a HASH - $hash"
+      unless ref $hash eq 'HASH';
+
+    return $hash;
+}
+
+sub build {
+    my ( $self, %args ) = @_;
+
+    # Get an easy access reference to the data
+    my $data_ref = $self->data;
+
+    # Create a private sub that searches for a file in all the paths
+    # specified in $self->path
+    my $find = sub {
+        my $name = shift;
+        my @paths = ref( $self->path ) ? @{ $self->path } : ( $self->path );
+        for my $path (@paths) {
+            next unless defined $path;
+            my $filename = sprintf( '%s/%s.%s', $path, $name, $self->ext );
+            return $filename if -r $filename;
+        }
+    };
+
+    # Create a private sub that parses a config file
+    my $process = sub {
+        my $name   = shift;
+        my $parsed = {};
+        try {
+            $parsed = $self->load($name);
+        }
+        catch {
+            croak "Parsing $name died with error: '${_}'";
+        };
+        $data_ref = _merge( $data_ref, $parsed );
+    };
+
+    # Find, parse and merge 'config' and mode files
+    for ( 'config', $self->app->mode ) {
+        if ( my $filename = $find->($_) ) {
+            $process->($filename);
+        }
+    }
+
+    # Register two methods: config and config_hash
+    $self->register(
+        config_hash => $self->data,
+        config      => sub {
+            my ( $app, $path ) = @_;
+            return $self->get($path);
+        }
+    );
+}
+
+sub _merge {
+    my ( $a, $b ) = @_;
+
+    return $b
+      if !ref($a)
+      || !ref($b)
+      || ref($a) ne ref($b)
+      || ref($a) ne 'HASH';
+
+    for my $k ( keys %$b ) {
+        $a->{$k} =
+          exists $a->{$k}
+          ? _merge( $a->{$k}, $b->{$k} )
+          : $b->{$k};
+    }
+
+    return $a;
 }
 
 1;
@@ -125,12 +193,25 @@ Kelp::Module::Config - Configuration for Kelp applications
 =head1 DESCRIPTION
 
 This is one of the two modules that are automatically loaded for each and every
-Kelp application. It uses L<Config::Hash> to read Perl-style hashes from files
-and merge them depending on the value of the C<mode> attribute.
+Kelp application. It reads configuration files containing Perl-style hashes,
+and merges them depending on the value of the application's C<mode> attribute.
 
 The main configuration file name is C<config.pl>, and it will be searched in the
-C<conf> directory or C<../conf>. The latter is convenient for running tests
-which use the same configuration settings as the main app.
+C<conf> and C<../conf> directories. You can also set the C<KELP_CONFIG_DIR> environmental
+variable with the path to the configuration files.
+
+This module comes with some L<default values|/DEFAULTS>, so if there are no
+configuration files found, those values will be used.
+Any values from configuration files will add to or override the default values.
+
+=head1 ORDER
+
+First the module will look for C<conf/config.pl>, then for C<../conf/config.pl>.
+If found, they will be parsed and merged into the default values.
+The same order applies to the I<mode> file too, so if the application
+L<mode|Kelp/mode> is I<development>, then C<conf/development.pl> and
+C<../conf/development.pl> will be looked for. If found, they will also be merged
+to the config hash.
 
 =head1 REGISTERED METHODS
 
@@ -138,7 +219,7 @@ This module registers the following methods into the underlying app:
 
 =head2 config
 
-A wrapper for the C<get> method in L<Config::Hash>.
+A wrapper for the C</get> method.
 
     # Somewhere in the app
     my $pos = $self->config('row.col.position');
@@ -152,6 +233,53 @@ A reference to the entire configuration hash.
     my $pos = $self->config_hash->{row}->{col}->{position};
 
 Using this or C<config> is entirely up to the application developer.
+
+=head1 ATTRIBUTES
+
+This module implements some attributes, which can be overridden by subclasses.
+
+=head2 ext
+
+The file extension of the configuration files. Default is C<pl>.
+
+=head2 separator
+
+A regular expression for the value separator used by L</get>. The default is
+C<qr/\./>, i.e. a dot.
+
+=head2 path
+
+Specifies a path, or an array of paths where to look for configuration files.
+This is particularly useful when writing tests, because you can set a custom
+path to a peculiar configuration.
+
+=head2 data
+
+The hashref with data contained in all of the merged configurations.
+
+=head1 METHODS
+
+The module also implements some methods for parsing the config files, which can
+be overridden in extending classes.
+
+=head2 get
+
+C<get($string)>
+
+Get a value from the config using a separated string.
+
+    my $value = $c->get('bar.foo.baz');
+    my $same  = $c->get('bar')->{foo}->{baz};
+    my $again = $c->data->{bar}->{foo}->{baz};
+
+By default the separator is a dot, but this can be changed via the
+L</separator> attribute.
+
+=head2 load
+
+C<load(filename)>
+
+Loads, and parses the file C<$filename> and returns a hash reference.
 
 =head1 DEFAULTS
 
@@ -171,7 +299,7 @@ C<http://localhost:5000>
 =head2 modules
 
 An arrayrf with module names to load on startup. The default value is
-C<['JSON', 'Template', 'Logger']>
+C<['JSON', 'Template']>
 
 =head2 modules_init
 
@@ -185,6 +313,44 @@ empty array.
 
 =head2 middleware_init
 
-A hashref with iitialization arguments for each of the loaded middleware.
+A hashref with initialization arguments for each of the loaded middleware.
+
+=head1 SUBCLASSING
+
+You can subclass this module and use other types of configuration files
+(for example YAML). You need to override the C<ext> attribute
+and the C<load> subroutine.
+
+    package Kelp::Module::Config::Custom;
+    use Kelp::Parent 'Kelp::Module::Config';
+
+    # Set the config file extension to .cus
+    attr ext => 'cus';
+
+    sub load {
+        my ( $self, $filename ) = @_;
+
+        # Load $filename, parse it and return a hashref
+    }
+
+    1;
+
+Later ...
+
+    # app.psgi
+    use MyApp;
+
+    my $app = MyApp->new( config_module => 'Config::Custom' );
+
+    run;
+
+The above example module will look for C<config/*.cus> to load as configuration.
+
+=head1 TESTING
+
+Since the config files are searched in both C<conf/> and C<../conf/>, you can
+use the same configuration set of files for your application and for your tests.
+Assuming the all of your test will reside in C<t/>, they should be able to load
+and find the config files at C<../conf/>.
 
 =cut
